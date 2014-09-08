18da470ae7b7e68522aec1517e5d160cb3828af6
Viliam Repan <vilo.repan@evolveum.com>
2014-09-08 23:13:20 +0200

    user list finished, user details in progress.

--- src/main/java/sk/lazyman/gizmo/web/app/PageUser.java
+++ src/main/java/sk/lazyman/gizmo/web/app/PageUser.java
@@ -7,4 +7,6 @@ import org.wicketstuff.annotation.mount.MountPath;
  */
 @MountPath("/app/user")
 public class PageUser extends PageAppTemplate {
+
+    public static final String USER_ID = "userId";
 }
--- src/main/java/sk/lazyman/gizmo/web/app/PageUsers.java
+++ src/main/java/sk/lazyman/gizmo/web/app/PageUsers.java
@@ -1,11 +1,16 @@
 package sk.lazyman.gizmo.web.app;
 
+import org.apache.wicket.ajax.AjaxRequestTarget;
 import org.apache.wicket.extensions.markup.html.repeater.data.table.IColumn;
+import org.apache.wicket.extensions.markup.html.repeater.data.table.PropertyColumn;
 import org.apache.wicket.extensions.markup.html.repeater.util.SortableDataProvider;
+import org.apache.wicket.model.IModel;
+import org.apache.wicket.request.mapper.parameter.PageParameters;
 import org.wicketstuff.annotation.mount.MountPath;
 import sk.lazyman.gizmo.component.LinkColumn;
 import sk.lazyman.gizmo.component.TablePanel;
 import sk.lazyman.gizmo.component.UsersDataProvider;
+import sk.lazyman.gizmo.data.User;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -26,9 +31,26 @@ public class PageUsers extends PageAppTemplate {
         SortableDataProvider provider = new UsersDataProvider(getUserRepository());
         List<IColumn> columns = new ArrayList<>();
 
-        columns.add(new LinkColumn(createStringResource("Username"), "userName"));
+        columns.add(new LinkColumn<User>(createStringResource("PageUsers.table.username"), "userName") {
 
-        TablePanel table = new TablePanel(ID_TABLE, provider, columns, 20);
+            @Override
+            public void onClick(AjaxRequestTarget target, IModel<User> rowModel) {
+                userDetailsPerformed(target, rowModel.getObject());
+            }
+        });
+        columns.add(new PropertyColumn(createStringResource("PageUsers.table.firstName"), "firstName"));
+        columns.add(new PropertyColumn(createStringResource("PageUsers.table.lastName"), "lastName"));
+        columns.add(new PropertyColumn(createStringResource("PageUsers.table.email"), "email"));
+        columns.add(new PropertyColumn(createStringResource("PageUsers.table.role"), "role"));
+
+        TablePanel table = new TablePanel(ID_TABLE, provider, columns, 15);
         add(table);
+    }
+
+    private void userDetailsPerformed(AjaxRequestTarget target, User user) {
+        PageParameters params = new PageParameters();
+        params.set(PageUser.USER_ID, user.getId());
+
+        setResponsePage(PageUser.class, params);
     }
 }
