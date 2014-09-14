22d68a19fef11601a5ad947a8ec897ef4c5e7f8a
Viliam Repan <vilo.repan@evolveum.com>
2014-09-14 14:48:12 +0200

    email log searching implemented.

--- src/main/java/sk/lazyman/gizmo/data/provider/EmailDataProvider.java
+++ src/main/java/sk/lazyman/gizmo/data/provider/EmailDataProvider.java
@@ -1,5 +1,9 @@
 package sk.lazyman.gizmo.data.provider;
 
+import com.mysema.query.BooleanBuilder;
+import com.mysema.query.types.Predicate;
+import com.mysema.query.types.PredicateOperation;
+import com.mysema.query.types.expr.BooleanExpression;
 import org.apache.wicket.extensions.markup.html.repeater.util.SortableDataProvider;
 import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.Model;
@@ -7,11 +11,13 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import sk.lazyman.gizmo.data.EmailLog;
+import sk.lazyman.gizmo.data.QEmailLog;
 import sk.lazyman.gizmo.dto.EmailFilterDto;
 import sk.lazyman.gizmo.repository.EmailLogRepository;
 
 import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 
 /**
  * @author lazyman
@@ -30,7 +36,7 @@ public class EmailDataProvider extends SortableDataProvider<EmailLog, String> {
         Sort sort = new Sort(Sort.Direction.DESC, EmailLog.F_DATE);
 
         PageRequest page = new PageRequest((int) first, (int) count, sort);
-        Page<EmailLog> found = emailRepository.findAll(page);
+        Page<EmailLog> found = emailRepository.findAll(createPredicate(), page);
         if (found != null) {
             return found.iterator();
         }
@@ -50,5 +56,28 @@ public class EmailDataProvider extends SortableDataProvider<EmailLog, String> {
 
     public void setFilter(EmailFilterDto filter) {
         this.filter = filter;
+    }
+
+    private Predicate createPredicate() {
+        if (filter == null
+                || (filter.getFrom() == null && filter.getTo() == null && filter.getSender() == null)) {
+            return null;
+        }
+
+        List<Predicate> list = new ArrayList<>();
+        if (filter.getFrom() != null) {
+            list.add(QEmailLog.emailLog.date.goe(filter.getFrom()));
+        }
+
+        if (filter.getTo() != null) {
+            list.add(QEmailLog.emailLog.date.loe(filter.getTo()));
+        }
+
+        if (filter.getSender() != null) {
+            list.add(QEmailLog.emailLog.sender.eq(filter.getSender()));
+        }
+
+        BooleanBuilder bb = new BooleanBuilder();
+        return bb.orAllOf(list.toArray(new Predicate[list.size()]));
     }
 }
--- src/main/java/sk/lazyman/gizmo/repository/EmailLogRepository.java
+++ src/main/java/sk/lazyman/gizmo/repository/EmailLogRepository.java
@@ -1,11 +1,12 @@
 package sk.lazyman.gizmo.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QueryDslPredicateExecutor;
 import sk.lazyman.gizmo.data.EmailLog;
 
 /**
  * @author lazyman
  */
-public interface EmailLogRepository extends JpaRepository<EmailLog, Integer> {
+public interface EmailLogRepository extends JpaRepository<EmailLog, Integer>, QueryDslPredicateExecutor<EmailLog> {
 
 }
